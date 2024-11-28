49c5eace2ac3d88333b0948daff5dd709b256d99
masterlangueetinformatique <masterlangueetinformatique@vbox>
2024-11-28 13:35:24 +0100

    chat

---
+++ atelier06/Chat.java
@@ -0,0 +1,26 @@
+package atelier06;
+
+public class Chat extends Felin implements Domesticable {
+	
+	private String nom;
+
+	/** le constructeur de Chat fait appel au constructeur de la sur-classe Felin */
+	public Chat(String type) { // les chats ont 4 pattes
+		super(type);
+		domestiquer("Titus");
+	}
+
+	/** Domestiquer un animal et lui donner un nom
+	 * @param nom nom de l'animal */
+	public void domestiquer(String nom) {
+		domestique = true;
+		this.nom = nom;
+	}
+
+	/** Rend le nom de l'animal
+	 * @return nom de l'animal */
+	public String nom() {
+		return nom;
+	}
+
+}
---
+++ atelier06/testChat.java
@@ -0,0 +1,11 @@
+package atelier06;
+
+public class testChat {
+	/** point d'entrée d'un exécutable */
+	public static void main(String[] args) {
+		Chat c = new Chat("Abyssins");
+		c.présente();
+		c.crie();
+	}
+
+}
